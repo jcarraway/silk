0fc13f6e723e94b252242e7f402f79f3a383edf5
Jarrett Carraway <jarrett.s.carraway@gmail.com>
2019-03-03 15:58:13 -0500

    wompwompwomp - can't properly transpile common package for use on web

--- packages/web/babel.config.js
+++ packages/web/babel.config.js
@@ -4,15 +4,16 @@ module.exports = api => {
   const presets = ['next/babel', '@zeit/next-typescript/babel'];
 
   const plugins = [
-    [
-      'module-resolver',
-      {
-        root: ['./'],
-        alias: {
-          '^react-native$': 'react-native-web',
-        },
-      },
-    ],
+    // [
+    //   'module-resolver',
+    //   {
+    //     root: ['./'],
+    //     alias: {
+    //       '^react-native$': 'react-native-web',
+    //     },
+    //   },
+    // ],
+    ['react-native-web', { commonjs: true }],
   ];
 
   return {
